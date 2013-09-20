84fe767678bb9c4e8b2f0254753f0b41874029f6
Santiago Vanegas <savanegasg@gmail.com>
2013-09-20 17:54:47 -0500

    ACIS / REDIS 2013 Problems accepted

--- ACIS REDIS 2013 - Colombia/F - Shuffling Cards/cards.cpp
+++ ACIS REDIS 2013 - Colombia/F - Shuffling Cards/cards.cpp
@@ -23,57 +23,57 @@ using namespace std;
 const int MAXN = 1045;
 int f[MAXN];
 int act[MAXN];
+bool visit[MAXN];
 int n;
 unsigned long long r;
-//int iteraciones [MAXN][MAXN];
-vector <vector <int> > iteraciones;
 
 int
-iterar() {
-    int cont = 0;
-    bool iguales = false;         
-    while (!iguales) {
-        iguales = true;
-        vector <int> posiciones;
-        for (int i = 1; i <= n; i++) {
-            int pos = f[act[i]];
-            act[i] = pos;
-            //iteraciones[cont+1][i] = pos;
-            posiciones.push_back(pos);
-            if (pos != i) iguales = false;
-        }
-        iteraciones.push_back(posiciones);
-        cont++;
-    }
-    return cont;
+iterar(int pos) {
+    if (visit[pos]) return 0;
+    visit[pos] = true;
+    return 1 + iterar(f[pos]);
+}
+
+void
+actualizar(int pos, int inicial) {
+    int next = f[act[pos]];
+    act[pos] = next;
+    if (next != inicial) actualizar(f[pos], inicial);
+    
 }
 
 int
 main() {
     while (cin >> n >> r) {
-        iteraciones.clear();
-        vector <int> ini;
         for (int i = 1; i <= n; i++) {
             int x; cin >> x;
             f[i] = x;
             act[i] = i;
-            //iteraciones[0][i] = i;
-            ini.push_back(i);
+            visit[i] = false;
         }
-        iteraciones.push_back(ini);
         if (r == 0) {
-            cout << iteraciones[0][0];
+            cout << act[1];
             for (int i = 2; i <= n; i++) {
-                cout << " " << iteraciones[0][i-1];   
-            }   
+                cout << " " << act[i];   
+            }
+            puts("");
+            continue;
         }
         else {
-            int it = iterar();
-            int j = r%it;
-            cout << iteraciones[j][0];
-            for (int i = 2; i <= n; i++) {
-                cout << " " << iteraciones[j][i-1];   
+            for (int i = 1; i <= n; i++) {
+                if (!visit[i]) {
+                    int j = iterar(i);
+                    int demas = r%j;
+                    for (int k = 0; k < demas; k++) {
+                        actualizar(i, act[i]);
+                        
+                    }
+                }   
             }
+        }
+        cout << act[1];
+        for (int i = 2; i <= n; i++) {
+            cout << " " << act[i]; 
         }
         puts("");
     }
